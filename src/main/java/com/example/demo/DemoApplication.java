2a63ebc6620bb676622d0a936520e088767fa418
sanix <sanicksikani@gmail.com>
2020-09-25 07:00:51 +0300

    Create POJO class with @Component Annotation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.config.SequenceGeneratorConfiguration;
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,15 +10,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(DemoApplication.class, args);
-		ApplicationContext context=new AnnotationConfigApplicationContext(
-				SequenceGeneratorConfiguration.class
-		);
 
-		SequenceGenerator generator =
-				(SequenceGenerator) context.getBean("sequenceGenerator", SequenceGenerator.class);
-		//If there is only a single bean, you can omit the bean name.
-		System.out.println(generator.getSequence());
-		System.out.println(generator.getSequence());
 	}
 
 }
---
+++ src/main/java/com/example/demo/SequenceDao.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public interface SequenceDao {
+
+    public Sequence getSequence(String sequenceId);
+    public int getNextValue(String sequenceId);
+}
