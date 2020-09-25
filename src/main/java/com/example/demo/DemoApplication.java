232805eb2295b6a98e8439deae482c3816ad40f7
sanix <sanicksikani@gmail.com>
2020-09-25 06:42:48 +0300

    Get POJO Instances or Beans from the IoC Container

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.config.SequenceGeneratorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context=new AnnotationConfigApplicationContext(
+				SequenceGeneratorConfiguration.class
+		);
+
+		SequenceGenerator generator =
+				(SequenceGenerator) context.getBean("sequenceGenerator", SequenceGenerator.class);
+		//If there is only a single bean, you can omit the bean name.
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());
 	}
 
 }
